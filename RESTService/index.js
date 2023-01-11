0e9bf5d6d7ac94b744594345dee54abbfccf59e0
Desmarchelier Maxime <maxime.desmarchelier@ensiie.fr>
2023-01-11 19:44:49 +0100

    Include RestService to docker-compose.yml

--- RESTService/index.js
+++ RESTService/index.js
@@ -8,8 +8,8 @@ app.use('/tests', require('./routes/tests'));
 
 (async () => {
     try {
-        app.listen(3000);
-        console.log('Server started on port 3000');
+        app.listen(process.env.EXTERNAL_PORT);
+        console.log('Server started on port ' + process.env.EXTERNAL_PORT);
     } catch (error) {
         console.log(error);
     }

e1562d15a77f06714a9739d390f10848480ff783
Desmarchelier Maxime <maxime.desmarchelier@ensiie.fr>
2023-01-12 00:49:23 +0100

    Create HelloWorld SOAP Service

---
+++ SOAPService/src/main/java/com/SOAPService/HelloWorldEndpoint.java
@@ -0,0 +1,26 @@
+package com.SOAPService;
+
+import io.spring.guides.gs_producing_web_service.GetHelloWorldRequest;
+import io.spring.guides.gs_producing_web_service.GetHelloWorldResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class HelloWorldEndpoint {
+    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+    @Autowired
+    public HelloWorldEndpoint() {
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getHelloWorldRequest")
+    @ResponsePayload
+    public GetHelloWorldResponse getHelloWorld(@RequestPayload GetHelloWorldRequest request) {
+        GetHelloWorldResponse response = new GetHelloWorldResponse();
+        response.setHello("Hello " + request.getName());
+        return response;
+    }
+}
---
+++ SOAPService/src/main/java/com/SOAPService/ProducingWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.SOAPService;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducingWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.SOAPService.ProducingWebServiceApplication.class, args);
+    }
+}
